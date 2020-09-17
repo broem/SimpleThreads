0e8a0f5adec99d60bf2017640ed66920c0d9372a
Bleach <wacbleach@gmail.com>
2020-09-17 00:49:07 -0400

    more tests for overlaps, etc

--- test/projectSetTests.py
+++ test/projectSetTests.py
@@ -39,6 +39,43 @@ class ProjectsSetTests(unittest.TestCase):
         resp = project.Project.getReimbursement(projectSet=mySet)
 
         self.assertTrue(resp == 590)
+    
+    def test_set_with_consecutive_singles_mixed(self):
+        ok = project.Project(name='project1', cityType=cityInfo.CityType.low, dateStart=datetime.strptime('9/1/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/1/15', '%m/%d/%y'))
+        ok1 = project.Project(name='project2', cityType=cityInfo.CityType.low, dateStart=datetime.strptime('9/1/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/1/15', '%m/%d/%y'))
+        ok2 = project.Project(name='project3', cityType=cityInfo.CityType.high, dateStart=datetime.strptime('9/2/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/2/15', '%m/%d/%y'))
+        ok3 = project.Project(name='project3', cityType=cityInfo.CityType.high, dateStart=datetime.strptime('9/2/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/3/15', '%m/%d/%y'))
+        mySet = [ok, ok1, ok2, ok3]
+        resp = project.Project.getReimbursement(projectSet=mySet)
+
+        self.assertTrue(resp == 185)
+    
+    def test_consecutive_highs_mixed(self):
+        ok = project.Project(name='project1', cityType=cityInfo.CityType.low, dateStart=datetime.strptime('9/1/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/3/15', '%m/%d/%y'))
+        ok1 = project.Project(name='project2', cityType=cityInfo.CityType.high, dateStart=datetime.strptime('9/5/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/7/15', '%m/%d/%y'))
+        ok2 = project.Project(name='project3', cityType=cityInfo.CityType.high, dateStart=datetime.strptime('9/8/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/8/15', '%m/%d/%y'))
+        mySet = [ok1, ok, ok2]
+        resp = project.Project.getReimbursement(projectSet=mySet)
+        
+        self.assertTrue(resp == 445)
+
+    def test_mixed_overlap_high_left(self):
+        ok = project.Project(name='project1', cityType=cityInfo.CityType.low, dateStart=datetime.strptime('9/1/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/1/15', '%m/%d/%y'))
+        ok1 = project.Project(name='project2', cityType=cityInfo.CityType.high, dateStart=datetime.strptime('9/3/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/5/15', '%m/%d/%y'))
+        ok2 = project.Project(name='project3', cityType=cityInfo.CityType.low, dateStart=datetime.strptime('9/4/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/8/15', '%m/%d/%y'))
+        mySet = [ok1, ok, ok2]
+        resp = project.Project.getReimbursement(projectSet=mySet)
+        
+        self.assertTrue(resp == 465)
+
+    def test_mixed_overlap_high_right(self):
+        ok = project.Project(name='project1', cityType=cityInfo.CityType.low, dateStart=datetime.strptime('9/1/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/1/15', '%m/%d/%y'))
+        ok1 = project.Project(name='project2', cityType=cityInfo.CityType.low, dateStart=datetime.strptime('9/3/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/5/15', '%m/%d/%y'))
+        ok2 = project.Project(name='project3', cityType=cityInfo.CityType.high, dateStart=datetime.strptime('9/4/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/8/15', '%m/%d/%y'))
+        mySet = [ok1, ok, ok2]
+        resp = project.Project.getReimbursement(projectSet=mySet)
+        
+        self.assertTrue(resp == 485)
 
 
 if __name__ == '__main__':
