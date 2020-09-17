b1c0576e8c5e7a615b87c187918f0a96173f1c86
Bleach <wacbleach@gmail.com>
2020-09-17 00:56:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-from datetime import datetime, date, timedelta
-import project as project
-import cityInfo as cityInfo
-
 if __name__ == "__main__":
-    # build a project
-    ok = project.Project(name='project1', cityType=cityInfo.CityType.low, dateStart=datetime.strptime('9/1/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/1/15', '%m/%d/%y'))
-    ok1 = project.Project(name='project2', cityType=cityInfo.CityType.low, dateStart=datetime.strptime('9/1/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/1/15', '%m/%d/%y'))
-    ok2 = project.Project(name='project3', cityType=cityInfo.CityType.low, dateStart=datetime.strptime('9/2/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/2/15', '%m/%d/%y'))
-    ok3 = project.Project(name='project3', cityType=cityInfo.CityType.low, dateStart=datetime.strptime('9/2/15', '%m/%d/%y'), dateEnd=datetime.strptime('9/3/15', '%m/%d/%y'))
-    mySet = [ok, ok1, ok2, ok3]
+    # the readme should contain some info on running this project's tests
 
-    yo = project.Project.getReimbursement(projectSet=mySet)
-
-    print(yo)
+    print("readme.md")
